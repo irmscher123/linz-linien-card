0700599f82369a148203fb54a39a67b93ad3e122
Irmscher <irmsch@gmail.com>
2026-04-19 20:18:00 +0200

    Update image URLs in linz-linien-combined.js

--- linz-linien-combined.js
+++ linz-linien-combined.js
@@ -279,7 +279,7 @@ class LinzMonitorCombined extends HTMLElement {
         </style>
         <ha-card>
           <div class="header-box">
-            <img class="stop-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Zeichen_224_-_Haltestelle%2C_StVO_2017.svg/1200px-Zeichen_224_-_Haltestelle%2C_StVO_2017.svg.png">
+            <img class="stop-logo" src="https://upload.wikimedia.org/wikipedia/commons/a/a6/Zeichen_224_-_Haltestelle%2C_StVO_2017.svgg">
             <div class="stop-title"></div>
           </div>
           <div class="rows-container"></div>
@@ -459,7 +459,7 @@ class LinzMonitorCombined extends HTMLElement {
         </style>
         <ha-card>
           <div class="title-area">
-            <img class="title-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Zeichen_224_-_Haltestelle%2C_StVO_2017.svg/1200px-Zeichen_224_-_Haltestelle%2C_StVO_2017.svg.png">
+            <img class="title-icon" src="https://upload.wikimedia.org/wikipedia/commons/a/a6/Zeichen_224_-_Haltestelle%2C_StVO_2017.svgg">
             <span class="stop-name"></span>
           </div>
           <table><tbody id="list"></tbody></table>
@@ -610,7 +610,7 @@ class LinzMonitorCombined extends HTMLElement {
         </style>
         <ha-card>
           <div class="title-area">
-            <img class="title-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Zeichen_224_-_Haltestelle%2C_StVO_2017.svg/1200px-Zeichen_224_-_Haltestelle%2C_StVO_2017.svg.png">
+            <img class="title-icon" src="https://upload.wikimedia.org/wikipedia/commons/a/a6/Zeichen_224_-_Haltestelle%2C_StVO_2017.svgg">
             <span class="stop-name"></span>
           </div>
           <table><tbody id="list"></tbody></table>
